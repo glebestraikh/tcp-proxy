8cb2020c64638b3dcac313548e85d2e04e96e538
Gleb Gleb <g.estraikh@gmail.com>
2025-11-04 21:27:19 +0700

    handle errors in relay.go

--- internal/adapter/in/relay.go
+++ internal/adapter/in/relay.go
@@ -17,15 +17,29 @@ func (s *ServerAdapter) relay(client, target net.Conn) {
 				slog.Any("target_addr", target.RemoteAddr()),
 				slog.Any("err", err),
 			)
-			return
 		}
-		target.(*net.TCPConn).CloseWrite()
+		if tcp, ok := target.(*net.TCPConn); ok {
+			if err := tcp.CloseWrite(); err != nil {
+				slog.Error("Error closing write on target", slog.Any("err", err))
+			}
+		}
 		done <- struct{}{}
 	}()
 
 	go func() {
-		io.Copy(client, target)
-		client.(*net.TCPConn).CloseWrite()
+		_, err := io.Copy(client, target)
+		if err != nil {
+			slog.Error("Error relaying data from target to client",
+				slog.Any("client_addr", client.RemoteAddr()),
+				slog.Any("target_addr", target.RemoteAddr()),
+				slog.Any("err", err),
+			)
+		}
+		if tcp, ok := client.(*net.TCPConn); ok {
+			if err := tcp.CloseWrite(); err != nil {
+				slog.Error("Error closing write on client", slog.Any("err", err))
+			}
+		}
 		done <- struct{}{}
 	}()
 
