cbe533da867ca2cec59079ddac1822249bfb9e8f
Gleb Gleb <g.estraikh@gmail.com>
2025-11-15 10:00:24 +0700

    refactor relay function to use WaitGroup and do not handle unnecessary closing errors

--- internal/adapter/in/relay.go
+++ internal/adapter/in/relay.go
@@ -1,58 +1,31 @@
 package in
 
 import (
-	"errors"
 	"io"
 	"log/slog"
 	"net"
-	"strings"
+	"sync"
 )
 
 func (s *ServerAdapter) relay(client, target net.Conn) {
-	done := make(chan struct{}, 2)
+	var wg sync.WaitGroup
+	wg.Add(2)
 
-	go func() {
-		_, err := io.Copy(target, client)
-		if err != nil && !isIgnorableError(err) {
-			slog.Error("Error relaying data from client to target",
-				slog.Any("client_addr", client.RemoteAddr()),
-				slog.Any("target_addr", target.RemoteAddr()),
-				slog.Any("err", err),
-			)
-		}
-		if tcp, ok := target.(*net.TCPConn); ok {
-			if err := tcp.CloseWrite(); err != nil && !isIgnorableCloseError(err) {
-				slog.Error("Error closing write on target", slog.Any("err", err))
-			}
-		}
-		done <- struct{}{}
-	}()
+	go s.copyData(target, client, &wg)
+	go s.copyData(client, target, &wg)
 
-	go func() {
-		_, err := io.Copy(client, target)
-		if err != nil && !isIgnorableError(err) {
-			slog.Error("Error relaying data from target to client",
-				slog.Any("client_addr", client.RemoteAddr()),
-				slog.Any("target_addr", target.RemoteAddr()),
-				slog.Any("err", err),
-			)
-		}
-		if tcp, ok := client.(*net.TCPConn); ok {
-			if err := tcp.CloseWrite(); err != nil && !isIgnorableCloseError(err) {
-				slog.Error("Error closing write on client", slog.Any("err", err))
-			}
-		}
-		done <- struct{}{}
-	}()
-
-	<-done
-	<-done
+	wg.Wait()
 }
 
-func isIgnorableError(err error) bool {
-	return errors.Is(err, io.EOF) || strings.Contains(err.Error(), "connection reset by peer")
-}
+func (s *ServerAdapter) copyData(dest, src net.Conn, wg *sync.WaitGroup) {
+	defer wg.Done()
 
-func isIgnorableCloseError(err error) bool {
-	return strings.Contains(err.Error(), "socket is not connected")
+	_, err := io.Copy(dest, src)
+	if err != nil {
+		slog.Debug("Connection interrupted",
+			slog.Any("src", src.RemoteAddr()),
+			slog.Any("dest", dest.RemoteAddr()),
+			slog.Any("err", err),
+		)
+	}
 }
